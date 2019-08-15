832b23c2f001fd18eee8f9b3b7727faf86ac8a1a
andrefangeloni <andrefangeloni@gmail.com>
2019-08-15 16:35:02 -0300

    Mais ajustes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  env: {
-    es6: true,
-  },
-  extends: ["airbnb", "prettier", "prettier/react"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    __DEV__: "readonly",
-  },
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["react", "prettier-tslint"],
-  rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", "js"] }],
-    "import/prefer-default-export": "off",
-    "react/prop-types": 0,
-  },
-=======
   root: true,
-  extends: '@react-native-community',
->>>>>>> Melhoria na estilização
+  extends: "@react-native-community",
 };
