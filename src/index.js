e2112a825043c4cf7bfbf44b3b4c47a07a9cee6f
andrefangeloni <andrefangeloni@gmail.com>
2019-08-15 16:44:29 -0300

    Ajustes feito, app rodando

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import { StatusBar } from "react-native";
 import Routes from "./routes";
-import "./config/ReactotronConfig";
-=======
-import React from 'react';
-import {StatusBar} from 'react-native';
-import Routes from './routes';
->>>>>>> Melhoria na estilização
 
 export default function App() {
   return (
