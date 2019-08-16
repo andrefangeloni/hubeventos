45db2ba77bf4b012a0599089b775b04f6edfadc1
andrefangeloni <andrefangeloni@gmail.com>
2019-08-16 09:51:55 -0300

    Ajustes para tentar criar um novo evento pelo App

--- src/pages/AddEvent/index.js
+++ src/pages/AddEvent/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import api from "../../services/api";
 // import ImagePicker from "react-native-image-picker"
 import {
   SafeAreaView,
@@ -13,7 +14,9 @@ import styles from "./styles";
 
 const noUser = "Você precisa estar logado para criar eventos";
 
-export default function AddEvent() {
+export default function AddEvent({ navigation }) {
+  const [preview, setPreview] = useState(null);
+  const [image, setImage] = useState(null);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [place, setPlace] = useState("");
@@ -21,32 +24,115 @@ export default function AddEvent() {
   const [date, setDate] = useState("");
   const [hour, setHour] = useState("");
 
-  // function handleSelectImage() {
-  //   ImagePicker.showImagePicker(
-  //     {
-  //       title: "Selecionar imagem",
-  //     },
-  //     upload => {}
-  //   );
-  // }
+  function handleSelectImage() {
+    ImagePicker.showImagePicker(
+      {
+        title: "Selecionar imagem",
+      },
+      upload => {
+        if (upload.error) {
+          console.log("Error");
+        } else if (upload.didCancel) {
+          console.log("User canceled");
+        } else {
+          const preview = {
+            uri: `data:image/jpeg;base64,${upload.data}`,
+          };
+
+          let prefix;
+          let ext;
+
+          if (upload.fileName) {
+            [prefix, ext] = upload.fileName.split(".");
+            ext = ext.toLowerCase() === "heic" ? "jpg" : ext;
+          } else {
+            prefix = new Date().getTime();
+            ext = "jpg";
+          }
+
+          const image = {
+            uri: upload.uri,
+            type: upload.type,
+            name: `${prefix}.${ext}`,
+          };
+
+          setPreview(preview);
+        }
+      }
+    );
+  }
+
+  async function handleSubmit() {
+    try {
+      const data = new FormData();
+      // data.append("fileName", setImage);
+      data.append("name", name);
+      data.append("description", description);
+      data.append("place", place);
+      data.append("value", value);
+      data.append("date", date);
+      data.append("hour", hour);
+
+      const response = await api.post("/events", data);
+      console.log(response.data);
+      navigation.navigate("Feed");
+    } catch (err) {
+      console.log(err, "Deu erro");
+    }
+  }
 
   return (
     <ScrollView>
       <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Novo evento</Text>
         <View style={styles.imageContainer} />
-        <TouchableOpacity onPress={handleSelectImage} style={styles.button}>
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
           <Text style={styles.buttonText}>Escolher imagem</Text>
         </TouchableOpacity>
-        <TextInput placeholder="Nome do evento" style={styles.input} />
-        <TextInput placeholder="Descrição" style={styles.input} />
-        <TextInput placeholder="Local" style={styles.input} />
-        <TextInput placeholder="Valor do ingresso" style={styles.input} />
+
+        {/* {useState(preview) && (
+          <View style={styles.imageContainer} source={useState(preview)} />
+        )} */}
+
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do evento"
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Descrição"
+          value={description}
+          onChangeText={setDescription}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Local"
+          value={place}
+          onChangeText={setPlace}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Valor do ingresso"
+          value={value}
+          onChangeText={setValue}
+        />
         <View style={{ flexDirection: "row" }}>
-          <TextInput placeholder="Data" style={styles.inputData} />
-          <TextInput placeholder="Hora" style={styles.inputHora} />
+          <TextInput
+            style={styles.inputData}
+            placeholder="Data"
+            value={date}
+            onChangeText={setDate}
+          />
+          <TextInput
+            style={styles.inputHora}
+            placeholder="Hora"
+            value={hour}
+            onChangeText={setHour}
+          />
         </View>
-        <TouchableOpacity onPress={() => ""} style={styles.button}>
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
           <Text style={styles.buttonText}>Salvar</Text>
         </TouchableOpacity>
       </SafeAreaView>
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://192.168.15.12:3333",
+  headers: { "Content-Type": "multipart/form-data" },
 });
 
 export default api;
